69dad6fb86c736500467845f8e9429e85e0c4862
dvarshanidze <davittvarshanidze@gmail.com>
2024-04-26 12:11:40 +0400

    added c/c++ warning executable

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -47,3 +47,14 @@ require("lazy").setup({
     },
   },
 })
+
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+require("lspconfig").clangd.setup({
+  on_attach = on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
