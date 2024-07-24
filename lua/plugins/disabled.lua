e45d9c87cfaa6fb46f8059df9d7f03fa71d37d9e
dvarshanidze <davittvarshanidze@gmail.com>
2024-07-24 20:09:47 +0400

    cleaned up

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,9 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.icons", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
