4e4c4246c6033f56693aa6b95e00a866a8dfce3d
dvarshanidze <davittvarshanidze@gmail.com>
2024-08-04 00:29:28 +0400

    cc

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,5 +5,5 @@ return {
   { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
 }
