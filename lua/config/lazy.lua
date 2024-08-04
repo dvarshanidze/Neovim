292fa25bebff69d87d5defffd94dec62972ffe1e
dvarshanidze <davittvarshanidze@gmail.com>
2024-08-04 21:29:29 +0400

    cc

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -47,14 +47,3 @@ require("lazy").setup({
     },
   },
 })
-
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-require("lspconfig").clangd.setup({
-  on_attach = on_attach,
-  capabilities = cmp_nvim_lsp.default_capabilities(),
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
-})
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -6,4 +6,6 @@ return {
   { "goolord/alpha-nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
